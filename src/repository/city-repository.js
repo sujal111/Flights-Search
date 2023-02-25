6bdbd84789303dfd9effcb07b45aee650979fbb6
sujalmaiti <sujalmaiti123456@gmail.com>
2023-02-25 17:16:45 +0530

    Completed city rep

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 const { City } = require('../models/index');
 
 class CityRepository {
@@ -28,6 +30,56 @@ class CityRepository {
         }
     }
 
+    async updateCity(cityId, data) { // {name: "Prayagraj"}
+        try {
+            // The below approach also works but will not return updated object
+            // if we are using Pg then returning: true can be used, else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id: cityId
+            //     },
+            //      
+            // });
+            // for getting updated data in mysql we use the below approach
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getAllCities(filter) { // filter can be empty also
+        try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
+            const cities = await City.findAll();
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
 
 }
 
